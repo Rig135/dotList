b48eeb8a3bdba46b00472aff083627270092a87e
Rig135 <167835509+Rig135@users.noreply.github.com.>
2026-04-08 00:37:06 +0530

    todo dueDate and priority UI

--- src/renderTodo.js
+++ src/renderTodo.js
@@ -25,22 +25,50 @@ export default function renderTodo(array,projectName) {
 
     const div = document.createElement("div");
     div.classList.add("todoBox");
-    // div.innerHTML = `${item.title}  <br>  ${item.description}`;
-
+    
+    // checkbox
     const checkBox = document.createElement('input');
     checkBox.type = 'checkbox';
     div.appendChild(checkBox);
 
+    //todo title and description
     const todoContent = document.createElement('div');
     const todoTitle = document.createElement('p');
     todoTitle.style.fontSize = '20px';
     todoTitle.textContent = item.title;
+
+    todoContent.classList.add('TodoContent');
     todoContent.appendChild(todoTitle);
 
     const todoDesc = document.createElement('p');
     todoDesc.textContent = item.description;
     todoContent.appendChild(todoDesc);
+
     div.appendChild(todoContent);
+
+    //due Date element
+    const due = document.createElement('div');
+    due.textContent = `📅 Due: ${item.dueDate}`;
+    due.classList.add('dueDate');
+
+    div.appendChild(due);
+
+    // priority element
+    const priority = document.createElement('div');
+    priority.textContent = item.priority;
+    
+    if(item.priority == "High"){
+      priority.classList.add('High');
+    }
+    else if(item.priority == 'Medium'){
+      priority.classList.add('Medium');
+    }
+    else {
+      priority.classList.add('Low');
+    }
+
+    div.appendChild(priority);
+    
     container.appendChild(div);
   }
 }
