36a55246507eebf2dffb9e72e6a8d40d31ebca85
Rig135 <167835509+Rig135@users.noreply.github.com.>
2026-05-01 13:02:43 +0530

    bundle using webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,20 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        type: "javascript/auto", 
+      },
+    ],
   },
 };
